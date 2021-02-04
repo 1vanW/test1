ce6b6476eaa4b42720d8fccc9ee53b89f6860bf7
Ivan <vinevega99@gmail.com>
2021-02-04 20:54:07 +0400

    Это наш первый коммит

---
+++ app/js/main.js
@@ -0,0 +1,33 @@
+// 'use strict';
+
+a = 5;
+
+console.log(a);
+
+// alert("Hello world");
+
+//  let answer = confirm('Are you peple?');
+ 
+// console.log(answer);
+
+// let answer = +prompt('Are you peple?','yes');
+ 
+// console.log(typeof(answer));
+
+// console.log(typeof("arr" + "- object"));
+// console.log((4 +  +"- object"));
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log('2'=== 2);
+
+let isChecked = false,
+    isClose = false;
+
+    console.log(isChecked || !isClose);
